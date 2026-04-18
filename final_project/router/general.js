bc276b225b33feb69b9c3c2bf9fcc1538ed88fa4
kalpeshdevre007-a11y <kalpeshdevre007@gmail.com>
2026-04-18 16:26:17 +0530

    update general.js with promise callbacks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,7 +17,7 @@ public_users.post('/register', (req, res) => {
   return res.status(200).json({ message: 'User successfully registered. Now you can login' });
 });
 
-// Get the book list available in the shop using async/await
+// Get all books using async/await
 public_users.get('/', async (req, res) => {
   try {
     const bookList = await new Promise((resolve) => resolve(books));
@@ -27,48 +27,39 @@ public_users.get('/', async (req, res) => {
   }
 });
 
-// Get book details based on ISBN using async/await
-public_users.get('/isbn/:isbn', async (req, res) => {
-  try {
-    const isbn = req.params.isbn;
-    const book = await new Promise((resolve, reject) => {
-      if (books[isbn]) resolve(books[isbn]);
-      else reject(new Error('Book not found'));
-    });
-    return res.status(200).json(book);
-  } catch (error) {
-    return res.status(404).json({ message: error.message });
-  }
+// Get book details based on ISBN using Promise callbacks
+public_users.get('/isbn/:isbn', (req, res) => {
+  const isbn = req.params.isbn;
+  new Promise((resolve, reject) => {
+    if (books[isbn]) resolve(books[isbn]);
+    else reject({ message: 'Book not found' });
+  })
+  .then(book => res.status(200).json(book))
+  .catch(err => res.status(404).json(err));
 });
 
-// Get book details based on author using async/await
-public_users.get('/author/:author', async (req, res) => {
-  try {
-    const author = req.params.author;
-    const booksByAuthor = await new Promise((resolve, reject) => {
-      const result = Object.values(books).filter(book => book.author === author);
-      if (result.length > 0) resolve(result);
-      else reject(new Error('No books found for this author'));
-    });
-    return res.status(200).json(booksByAuthor);
-  } catch (error) {
-    return res.status(404).json({ message: error.message });
-  }
+// Get book details based on author using Promise callbacks
+public_users.get('/author/:author', (req, res) => {
+  const author = req.params.author;
+  new Promise((resolve, reject) => {
+    const result = Object.values(books).filter(b => b.author === author);
+    if (result.length > 0) resolve(result);
+    else reject({ message: 'No books found for this author' });
+  })
+  .then(result => res.status(200).json(result))
+  .catch(err => res.status(404).json(err));
 });
 
-// Get book details based on title using async/await
-public_users.get('/title/:title', async (req, res) => {
-  try {
-    const title = req.params.title;
-    const booksByTitle = await new Promise((resolve, reject) => {
-      const result = Object.values(books).filter(book => book.title === title);
-      if (result.length > 0) resolve(result);
-      else reject(new Error('No books found with this title'));
-    });
-    return res.status(200).json(booksByTitle);
-  } catch (error) {
-    return res.status(404).json({ message: error.message });
-  }
+// Get book details based on title using Promise callbacks
+public_users.get('/title/:title', (req, res) => {
+  const title = req.params.title;
+  new Promise((resolve, reject) => {
+    const result = Object.values(books).filter(b => b.title === title);
+    if (result.length > 0) resolve(result);
+    else reject({ message: 'No books found with this title' });
+  })
+  .then(result => res.status(200).json(result))
+  .catch(err => res.status(404).json(err));
 });
 
 // Get book review
